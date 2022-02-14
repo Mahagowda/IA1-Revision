15457575f0a1506ca0f82cca9e4a50bcebb1116b
Mahagowda <94665876+Mahagowda@users.noreply.github.com>
2022-02-14 06:48:26 +0000

    p3

---
+++ p3final.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int input_n()
+{
+  int n;
+  printf("Enter a number:");
+  scanf("%d",&n);
+  return n;
+}
+int sum_n(int n)
+{
+  int ans;
+  ans = 0;
+  for(int i=1;i<=n;i++)
+  {
+    ans+=i;
+  }
+  return ans;
+}
+void output(int n,int sum)
+{
+  for(int i=1;i<n;i++)
+  {
+    printf("%d+",i);
+  }
+  printf("%d is %d\n",n,sum);
+}
+int main()
+{
+  int n,sum;
+  n = input_n();
+  sum = sum_n(n);
+  output(n,sum);
+  return 0;
+}
