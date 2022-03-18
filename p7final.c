8fbfec0908bc394a6342ab42592e3ce3f42c88e5
Mahagowda <94665876+Mahagowda@users.noreply.github.com>
2022-03-18 15:57:43 +0000

    p7

--- p7final.c
+++ p7final.c
@@ -6,17 +6,13 @@ struct complex
 
 int main()
 {
-   struct complex a, b, c;
-
-   printf("Enter a and b where a + ib is the first complex number.\n");
-   scanf("%d%d", &a.real, &a.img);
-   printf("Enter c and d where c + id is the second complex number.\n");
-   scanf("%d%d", &b.real, &b.img);
-
-   c.real = a.real + b.real;
-   c.img = a.img + b.img;
-
-   printf("Sum of the complex numbers: (%d) + (%di)\n", c.real, c.img);
-
-   return 0;
+struct complex a, b, c;
+printf("Enter a and b where a + ib is the first complex number.\n");
+scanf("%d%d", &a.real, &a.img);
+printf("Enter c and d where c + id is the second complex number.\n");
+scanf("%d%d", &b.real, &b.img);
+c.real = a.real + b.real;
+c.img = a.img + b.img;
+printf("Sum of the complex numbers: (%d) + (%di)\n", c.real, c.img);
+return 0;
 }
